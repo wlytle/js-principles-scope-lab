e8fa42f23c1596d86c12d011717faa10c62ec598
wlytle <wlytle89@gmail.com>
2020-11-16 13:56:02 -0700

    commit tree 392990508711cad0f8c41c13350136cc4e0818e0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "jo";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "maybe bob";
+}
